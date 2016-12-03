c299f9e318f91f493765113f28cc04b63c17d1eb
treilly <treilly@savantgroup.com>
2016-12-03 09:55:20 -0500

    Removed CSRF token temporarily

--- pestilence/pestilence/urls.py
+++ pestilence/pestilence/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 from rest_framework_jwt.views import obtain_jwt_token
@@ -9,6 +10,6 @@ from pestilence.schema import schema  # noqa: F401
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^graphql/', GraphQLView.as_view(graphiql=True)),
+    url(r'^graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     url(r'^api-token-auth/', obtain_jwt_token),
 ]
